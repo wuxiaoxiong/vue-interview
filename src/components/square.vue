f198258845f8a76a61a2c32ca3b861594852b00c
吴潇雄 <wuxiaoxiong@wywk.cn>
2020-05-22 17:24:18 +0800

    show content after click

--- src/components/square.vue
+++ src/components/square.vue
@@ -1,14 +1,29 @@
 <template>
-  <div class="square">
-
+  <div class="square" @click="setContent">
+    {{content}}
   </div>
 </template>
 
 <script>
   export default {
-    name: "square"
+    name: "square",
+    data() {
+      return {
+        content: '',
+      }
+    },
+    methods: {
+      setContent() {
+        this.content = 'X';
+      },
+    }
   }
 </script>
 
 <style scoped lang="stylus">
+  .square
+    cursor pointer
+    display inline-flex
+    justify-content center
+    align-items center
 </style>
